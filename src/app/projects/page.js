d8a178e4fa24b5564555f1758adb67ca8eae828b
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-06-06 17:43:33 -0500

    lupdate projects page with git repo links

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,5 +1,33 @@
 import React from 'react';
 
 export default function ProjectsPage() {
-  return <div>Projects Page</div>;
+  const projects = [
+    {
+      name: 'Project 1',
+      githubLink: 'https://github.com/laperkins2/portfolio.git',
+    },
+    {
+      name: 'Project 2',
+      githubLink: 'https://laperkins2.github.io/level2Capstone/',
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Projects Page</h1>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.name}>
+            <a
+              href={project.githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {project.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
